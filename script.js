3d6fdd5a4d7a4ef9d02de4092477efb629c21ae5
gwenparadis <gwendelynparadis2@gmail.com>
2022-10-18 15:35:28 -0400

    initial js notes and basic html document formatting

---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+
+// global variables
+let score = "";
+let highScore = "";
+
+function questions() = {    
+
+    const questionOne = [{
+        question = "insert question here",
+        answers: ["a", "b", "c", "d"],
+        corrAnswer: 0-3,
+    }];
+
+    const questionTwo = [{
+        question = "insert question here",
+        answers: ["a", "b", "c", "d"],
+        corrAnswer: 0-3,
+    }];
+
+    const questionThree = [{
+        question = "insert question here",
+        answers: ["a", "b", "c", "d"],
+        corrAnswer: 0-3,
+    }];
+
+    const questionFour = [{
+        question = "insert question here",
+        answers: ["a", "b", "c", "d"],
+        corrAnswer: 0-3,
+    }];
+
+    //WHEN I answer a question
+    //THEN I am presented with another question
+    for (let i = 0; i < questions.Length; i++) {
+
+    };
+
+    WHEN I answer a question incorrectly
+    THEN time is subtracted from the clock
+    WHEN all questions are answered or the timer reaches 0
+    THEN the game is over
+    WHEN the game is over
+    
+    //THEN I can save my initials and my score
+    localStorage.setItem(highScore);
+    highScore.textContent = highScore;
+
+    return (score);
+});
+
+startButton.addEventListener("click", questions());
+    WHEN I click the start button
+    THEN a timer starts -- run timer(); ???
+    and I am presented with a question -- run questions(); ???
+
+*/
